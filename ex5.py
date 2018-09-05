d84964de18442eb9b7efe5f87d25881b8be3bfa4
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2018-09-05 15:38:02 +1000

    change cm and kg into inch and pound respectively

--- ex5.py
+++ ex5.py
@@ -16,3 +16,17 @@ print(f"His teeth are usually {my_teeth} depending on the coffee.")
 # This line is tricky, try to get it exactly right
 total = my_age + my_height + my_weight
 print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
+# Study drills 2
+# Convert cm and kg to respectively inches and pounds
+my_height_inch = my_height * 0.393701
+my_height_inch = round(my_height_inch, 2)
+my_weight_pound = my_weight * 2.20462
+my_weight_pound = round(my_weight_pound, 2)
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height_inch} inches tall.")
+print(f"He's {my_weight_pound} pounds heavy.")
+print("Actually, that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
