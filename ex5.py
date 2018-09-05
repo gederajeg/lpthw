4487c263ac641c106172da48fa63308aa4fb5a0c
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2018-09-05 11:38:17 +1000

    exercise 5, format string

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Gede P. W. Rajeg'
+my_age = 31
+my_height = 170 # cm
+my_weight = 65 # kg
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} cm tall.")
+print(f"He's {my_weight} kg heavy.")
+print("Actually, that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# This line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
