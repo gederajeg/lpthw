4d1b12f42df1899aa6fea7dc7b297b9777948193
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2018-09-05 11:41:33 +1000

    change 'my_' in the variable

--- ex5.py
+++ ex5.py
@@ -1,18 +1,18 @@
-my_name = 'Gede P. W. Rajeg'
-my_age = 31
-my_height = 170 # cm
-my_weight = 65 # kg
-my_eyes = 'Brown'
-my_teeth = 'White'
-my_hair = 'Black'
+name = 'Gede P. W. Rajeg'
+age = 31
+height = 170 # cm
+weight = 65 # kg
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Black'
 
-print(f"Let's talk about {my_name}.")
-print(f"He's {my_height} cm tall.")
-print(f"He's {my_weight} kg heavy.")
+print(f"Let's talk about {name}.")
+print(f"He's {height} cm tall.")
+print(f"He's {weight} kg heavy.")
 print("Actually, that's not too heavy.")
-print(f"He's got {my_eyes} eyes and {my_hair} hair.")
-print(f"His teeth are usually {my_teeth} depending on the coffee.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
 
 # This line is tricky, try to get it exactly right
-total = my_age + my_height + my_weight
-print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
