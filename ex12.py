62d7616124ed1f90797775ac3ee431f9f681e940
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2021-12-23 14:45:58 +0800

    More prompting people exercise.

---
+++ ex12.py
@@ -0,0 +1,11 @@
+# Prompting people
+
+# y = input("Name? ") 
+# the above code prompts people with "Name? " and put the input given into the variable "y"
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
