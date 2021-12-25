df8ac149b36bae316ca9cbc97596ec00bf7d9868
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2021-12-25 21:26:19 +0800

    Add question about city to ex12.py

--- ex12.py
+++ ex12.py
@@ -6,6 +6,7 @@
 age = input("How old are you? ")
 height = input("How tall are you? ")
 weight = input("How much do you weigh? ")
+city = input("In which city are you currently living? ") # after testing, no need to quote the response for the printing effect.
 
-print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+print(f"So, you're {age} old, {height} tall, {weight} heavy, and are living in {city}.")
 
